ac25644bd6989910d0f3ade077fd811693c928b5
cool168 <cool168@users.noreply.github.com>
2019-02-08 20:08:44 +0800

    Update frp.sh

--- frp.sh
+++ frp.sh
@@ -7,5 +7,5 @@ if [ $FRP_CLIENT = "yes" ]
 then
     /var/frp/frpc -c /var/frp/conf/frpc.ini
 else
-    /app/kcp/client_linux_amd64 -l 127.0.0.1:$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP >/dev/null 2>&1 &
+    /var/frp/frps -c /var/frp/conf/frps.ini
 fi
