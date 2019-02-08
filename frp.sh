7f659a0dcfd9ba3c257dda7fafbf1b2a1701de2a
cool168 <cool168@users.noreply.github.com>
2019-02-08 20:07:29 +0800

    Update frp.sh

--- frp.sh
+++ frp.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-echo ${LOCAL_PORT=12948}
+echo ${FRP_CLIENT='yes'}
 
 sleep 1
-if [ $SHOW_LOGS = "yes" ]
+if [ $FRP_CLIENT = "yes" ]
 then
-    /app/kcp/client_linux_amd64 -l 127.0.0.1:$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP &
+    FRP_CLIENT
 else
     /app/kcp/client_linux_amd64 -l 127.0.0.1:$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP >/dev/null 2>&1 &
 fi
-sleep 5
-ss-local -s 127.0.0.1 -p $LOCAL_PORT -b 0.0.0.0 -l $SS_LOCAL_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 $SS_UDP $SS_OTA
